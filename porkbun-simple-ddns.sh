2f97ab55d80c09ad08d5ae129b72ad654e07a25c
Noel Varelas <noel@noelvarelas.com>
2024-06-25 13:03:16 -0700

    fixed all shellcheck complaints

--- porkbun-simple-ddns.sh
+++ porkbun-simple-ddns.sh
@@ -71,11 +71,11 @@ api_cmd_edit_aaaa="{\"apikey\": \"$APIKEY\", \"secretapikey\": \"$SECRETAPIKEY\"
 # Get Existing IPv4 A Record
 if [ "$UPDATE_A" == "true" ]; then
   full_a_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_get" "$api_uri_get_a")
-  if [ $(echo "$full_a_record" | jq '.status') != '"SUCCESS"' ]; then
+  if [ "$(echo "$full_a_record" | jq '.status')" != '"SUCCESS"' ]; then
     echo "ERROR: API request to get existing A record failed."
     exit 1
   fi
-  if [ $(echo "$full_a_record" | jq '.records[0].content') == "null" ]; then
+  if [ "$(echo "$full_a_record" | jq '.records[0].content')" == "null" ]; then
     echo "ERROR: DNS A record does not exist for $fulldomain."
     exit 1
   fi
@@ -86,11 +86,11 @@ fi
 # Get Existing IPv6 AAAA Record
 if [ "$UPDATE_AAAA" == "true" ]; then
   full_aaaa_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_get" "$api_uri_get_aaaa")
-  if [ $(echo "$full_aaaa_record" | jq '.status') != '"SUCCESS"' ]; then
+  if [ "$(echo "$full_aaaa_record" | jq '.status')" != '"SUCCESS"' ]; then
     echo "ERROR: API request to get existing AAAA record failed."
     exit 1
   fi
-  if [ $(echo "$full_aaaa_record" | jq '.records[0].content') == "null" ]; then
+  if [ "$(echo "$full_aaaa_record" | jq '.records[0].content')" == "null" ]; then
     echo "ERROR: DNS AAAA record does not exist for $fulldomain."
     exit 1
   fi
@@ -103,7 +103,7 @@ a_changed="false"
 if [ "$UPDATE_A" == "true" ]; then
   if [ "$ipv4_address" != "$a_record" ]; then
     edit_a_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_edit_a" "$api_uri_edit_a")
-    if [ $(echo "$edit_a_record" | jq '.status') != '"SUCCESS"' ]; then
+    if [ "$(echo "$edit_a_record" | jq '.status')" != '"SUCCESS"' ]; then
       echo "ERROR: API request to edit A record failed."
       exit 1
     fi
@@ -119,7 +119,7 @@ aaaa_changed="false"
 if [ "$UPDATE_AAAA" == "true" ]; then
   if [ "$ipv6_address" != "$aaaa_record" ]; then
     edit_aaaa_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_edit_aaaa" "$api_uri_edit_aaaa")
-    if [ $(echo "$edit_aaaa_record" | jq '.status') != '"SUCCESS"' ]; then
+    if [ "$(echo "$edit_aaaa_record" | jq '.status')" != '"SUCCESS"' ]; then
       echo "ERROR: API request to edit AAAA record failed."
       exit 1
     fi
