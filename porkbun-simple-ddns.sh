c50b6dba95dc9470b7dfd3f51aff77de2dc1ea5a
Noel Varelas <noel@noelvarelas.com>
2024-06-25 12:55:59 -0700

    improve external log, shellcheck fixes

--- porkbun-simple-ddns.sh
+++ porkbun-simple-ddns.sh
@@ -71,11 +71,11 @@ api_cmd_edit_aaaa="{\"apikey\": \"$APIKEY\", \"secretapikey\": \"$SECRETAPIKEY\"
 # Get Existing IPv4 A Record
 if [ "$UPDATE_A" == "true" ]; then
   full_a_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_get" "$api_uri_get_a")
-  if [ $(echo $full_a_record | jq '.status') != '"SUCCESS"' ]; then
+  if [ $(echo "$full_a_record" | jq '.status') != '"SUCCESS"' ]; then
     echo "ERROR: API request to get existing A record failed."
     exit 1
   fi
-  if [ $(echo $full_a_record | jq '.records[0].content') == "null" ]; then
+  if [ $(echo "$full_a_record" | jq '.records[0].content') == "null" ]; then
     echo "ERROR: DNS A record does not exist for $fulldomain."
     exit 1
   fi
@@ -86,11 +86,11 @@ fi
 # Get Existing IPv6 AAAA Record
 if [ "$UPDATE_AAAA" == "true" ]; then
   full_aaaa_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_get" "$api_uri_get_aaaa")
-  if [ $(echo $full_aaaa_record | jq '.status') != '"SUCCESS"' ]; then
+  if [ $(echo "$full_aaaa_record" | jq '.status') != '"SUCCESS"' ]; then
     echo "ERROR: API request to get existing AAAA record failed."
     exit 1
   fi
-  if [ $(echo $full_aaaa_record | jq '.records[0].content') == "null" ]; then
+  if [ $(echo "$full_aaaa_record" | jq '.records[0].content') == "null" ]; then
     echo "ERROR: DNS AAAA record does not exist for $fulldomain."
     exit 1
   fi
@@ -103,7 +103,7 @@ a_changed="false"
 if [ "$UPDATE_A" == "true" ]; then
   if [ "$ipv4_address" != "$a_record" ]; then
     edit_a_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_edit_a" "$api_uri_edit_a")
-    if [ $(echo $edit_a_record | jq '.status') != '"SUCCESS"' ]; then
+    if [ $(echo "$edit_a_record" | jq '.status') != '"SUCCESS"' ]; then
       echo "ERROR: API request to edit A record failed."
       exit 1
     fi
@@ -119,7 +119,7 @@ aaaa_changed="false"
 if [ "$UPDATE_AAAA" == "true" ]; then
   if [ "$ipv6_address" != "$aaaa_record" ]; then
     edit_aaaa_record=$(curl -s -X POST -H "Content-Type: application/json" -d "$api_cmd_edit_aaaa" "$api_uri_edit_aaaa")
-    if [ $(echo $edit_aaaa_record | jq '.status') != '"SUCCESS"' ]; then
+    if [ $(echo "$edit_aaaa_record" | jq '.status') != '"SUCCESS"' ]; then
       echo "ERROR: API request to edit AAAA record failed."
       exit 1
     fi
@@ -131,20 +131,22 @@ if [ "$UPDATE_AAAA" == "true" ]; then
 fi
 
 # Write to External Log (if enabled)
-if [ "$ENABLE_CHANGELOG" == "true" ] && ([ "$a_changed" == "true" ] || [ "$aaaa_changed" == "true" ]); then
-  if [ ! -f "$CHANGELOG_FILE" ]; then
-    touch "$CHANGELOG_FILE"
+if [ "$ENABLE_CHANGELOG" == "true" ]; then
+  if [ "$a_changed" == "true" ] || [ "$aaaa_changed" == "true" ]; then
     if [ ! -f "$CHANGELOG_FILE" ]; then
-      echo "ERROR: Could not create changelog file."
-      exit 1
+      touch "$CHANGELOG_FILE"
+      if [ ! -f "$CHANGELOG_FILE" ]; then
+        echo "ERROR: Could not create changelog file."
+        exit 1
+      fi
     fi
+    date >> "$CHANGELOG_FILE"
+    if [ "$a_changed" == "true" ]; then
+      echo "DNS A record for $fulldomain changed from $a_record to $ipv4_address" >> "$CHANGELOG_FILE"
+    fi
+    if [ "$aaaa_changed" == "true" ]; then
+      echo "DNS AAAA record for $fulldomain changed from $aaaa_record to $ipv6_address" >> "$CHANGELOG_FILE"
+    fi
+    echo >> "$CHANGELOG_FILE"
   fi
-  date >> $CHANGELOG_FILE
-  if [ "$a_changed" == "true" ]; then
-    echo "DNS A record for $fulldomain changed from $a_record to $ipv4_address" >> $CHANGELOG_FILE
-  fi
-  if [ "$aaaa_changed" == "true" ]; then
-    echo "DNS AAAA record for $fulldomain changed from $aaaa_record to $ipv6_address" >> $CHANGELOG_FILE
-  fi
-  echo >> $CHANGELOG_FILE
 fi
